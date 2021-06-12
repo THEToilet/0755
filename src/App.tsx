430c62c878acf4172466d0b600fe0ff8d109ab8b
THEToilet <bp18015@shibaura-it.ac.jp>
2021-06-13 06:52:03 +0900

    Update tab icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-//import React from 'react';
 import './App.css';
 import TopPage from "./pages/TopPage";
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,8 +2,8 @@ const Header = () => {
     return (
         <div className="header">
             <img className="logo" src={process.env.PUBLIC_URL + '0755.gif'} alt="logo"/>
-            <div className="header-back"><h1></h1></div>
-            <div className="header-bottom"></div>
+            <div className="header-back"/>
+            <div className="header-bottom"/>
         </div>
     );
 };
