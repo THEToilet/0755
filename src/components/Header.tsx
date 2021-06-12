b30222ee51e0b511f9d4c85353f1078eeb2b6d34
THEToilet <bp18015@shibaura-it.ac.jp>
2021-06-13 05:04:43 +0900

    :+1:

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,9 @@
-
 const Header = () => {
     return (
         <div className="header">
-            <img src={process.env.PUBLIC_URL + '0755.gif'} alt="logo"/>
+            <img className="logo" src={process.env.PUBLIC_URL + '0755.gif'} alt="logo"/>
+            <div className="header-back"><h1></h1></div>
+            <div className="header-bottom"></div>
         </div>
     );
 };
