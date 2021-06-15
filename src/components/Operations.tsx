2e1e52d728056b0fedcb77b782a6cb4ba5f855a0
THEToilet <bp18015@shibaura-it.ac.jp>
2021-06-15 19:14:53 +0900

    Update Operations

--- src/components/Operations.tsx
+++ src/components/Operations.tsx
@@ -1,10 +1,33 @@
+import styled from "styled-components";
+
+const OperationsIcon = styled.div`
+  align-items: flex-end;
+  background-color: #c4c4c4;
+  border-radius: 15px;
+  display: flex;
+  height: 54px;
+  justify-content: flex-end;
+  min-width: 226px;
+  padding: 9px 28px;
+  position: relative;
+  text-align: right;
+`;
+
+const Icon = styled.img`
+  height: 30px;
+  margin-bottom: 1px;
+  //margin-left: 30px;
+  mix-blend-mode: normal;
+  width: 30px;
+`;
+
 const Operations = () => {
     return (
-        <div className="operations">
-            <img className="search" src={process.env.PUBLIC_URL + 'images/search_black_24dp.svg'} alt="search"/>
-            <img className="sort" src={process.env.PUBLIC_URL + 'images/sort_black_24dp.svg'} alt="sort"/>
-            <img className="list" src={process.env.PUBLIC_URL + 'images/view_list_black_24dp.svg'} alt="list"/>
-        </div>
+        <OperationsIcon>
+            <Icon src={process.env.PUBLIC_URL + 'images/search_black_24dp.svg'} alt="search"/>
+            <Icon src={process.env.PUBLIC_URL + 'images/sort_black_24dp.svg'} alt="sort"/>
+            <Icon src={process.env.PUBLIC_URL + 'images/view_list_black_24dp.svg'} alt="list"/>
+        </OperationsIcon>
     );
 }
 
