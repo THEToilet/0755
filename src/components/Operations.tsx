0556f370ac2c3f252cb92c1d88569c8b0cb0ea33
THEToilet <bp18015@shibaura-it.ac.jp>
2021-06-15 17:06:56 +0900

    Update Toppage

---
+++ src/components/Operations.tsx
@@ -0,0 +1,11 @@
+const Operations = () => {
+    return (
+        <div className="operations">
+            <img className="search" src="./search" alt="search"/>
+            <img className="sort" src="./sort" alt="sort"/>
+            <img className="list" src="./list" alt="list"/>
+        </div>
+    );
+}
+
+export default Operations;
--- src/pages/TopPage.tsx
+++ src/pages/TopPage.tsx
@@ -1,4 +1,6 @@
 import Header from "../components/Header";
+import Operations from "../components/Operations";
+import ImageGrid from "../components/ImageGrid";
 
 type TopPagePropsType = {
     setOperationName: string;
@@ -9,7 +11,8 @@ const TopPage = ({setOperationName}: TopPagePropsType) => {
         <div className="top-page">
             <Header/>
             <div className="operation-name">{setOperationName}</div>
-            
+            <Operations/>
+            <ImageGrid setImageList={["fsdf", "dfsa"]}></ImageGrid>
         </div>
     );
 };
